f28a677ae8ea9c98f30ee6276dec272f4128c538
skipperr254 <skipperr254@gmail.com>
2024-12-04 13:01:57 +0300

    Refactoring and directory restructuring

---
+++ v.0.0.1/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'> {/* on regular devices, margin of 16px/1rem on the x direction, on small devices, 10% */}
+      Hello world
+    </div>
+  )
+}
+
+export default App
