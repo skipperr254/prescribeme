3d871fccfef042cc1160c3f1cf94c6b5c42f02f7
skipperr254 <skipperr254@gmail.com>
2024-12-12 19:32:43 +0300

    Navbar 99% complete

--- v.0.0.0/frontend/src/components/Navbar.jsx
+++ v.0.0.0/frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from "react-router-dom"
+import { NavLink, useNavigate, Link } from "react-router-dom"
 
 import { assets } from "../assets/assets"
 import { useState } from "react"
@@ -47,13 +47,31 @@ const Navbar = () => {
             {
                 isLoggedIn ?
                 (
-                    <div className="flex items-center gap-2 cursor-pointer">
+                    <div className="flex items-center gap-2 cursor-pointer relative group">
                         <img className="w-8 rounded-full" src={assets.profile_pic} alt="Profile Pic" />
                         <img className="w-2.5" src={assets.dropdown_icon} alt="Dropdowm Icon" />
+
+                        <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 tracking-normal hidden group-hover:block">
+                            <div className="min-w-48 bg-stone-100 border rounded border-stone-200 flex flex-col p-4 gap-4 shadow-md">
+                                <p 
+                                    onClick={() => navigate("/profile")} 
+                                    className="hover:text-black"
+                                >
+                                    My Account
+                                </p>
+                                <p 
+                                    onClick={() => navigate("/appointments")} 
+                                    className="hover:text-black"
+                                >
+                                    My Appointments
+                                </p>
+                                <p onClick={() => { setIsLoggedIn(false); navigate("/") }} className="hover:text-black">Logout</p>
+                            </div>
+                        </div>
                     </div>
                 ):
                 <button 
-                    onClick={() => navigate("/login")} 
+                    onClick={() => setIsLoggedIn(true)} 
                     className="bg-primary text-white hidden md:block rounded-full py-2 px-7 border-2 border-primary hover:bg-white hover:text-primary">
                     SIGN IN
                 </button>
