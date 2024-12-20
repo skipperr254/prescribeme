f214c658997e61154bc602b4714b8981a19b8cad
skipperr254 <skipperr254@gmail.com>
2024-12-20 11:13:27 +0300

    Added the AppContext for doctors

--- v.0.0.0/frontend/src/main.jsx
+++ v.0.0.0/frontend/src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import AppContextProvider from "./context/AppContext";
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <App />
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
